af78c423bd90640a2557603453d4b36e9883d0d6
olya belykh <belykh.o.v@gmail.com>
2019-05-11 17:41:17 +0300

    implement useReducer to manage todos

--- src/components/todo-editor/index.js
+++ src/components/todo-editor/index.js
@@ -21,36 +21,37 @@ const initialTodos = [
   }
 ]
 
-const TodoEditor = _ => {
-    const todosReducer = (state, action) => {
-        const { type, payload } = action
-        switch (type) {
-            case 'ADD_TODO':
-                return state.concat({ ...payload.todo, id: uuid() });
-            case 'COMPLETE_TODO':
-                return state.map(todo => todo.id === payload.id ? { ...todo, complete: !todo.complete } : todo)
-            default:
-                return state
-        }
+const todosReducer = (state, action) => {
+    const { type, payload } = action
+    switch (type) {
+        case 'ADD_TODO':
+            return state.concat({ ...payload.todo, id: uuid() });
+        case 'COMPLETE_TODO':
+            return state.map(todo => todo.id === payload.id ? { ...todo, complete: !todo.complete } : todo)
+        default:
+            return state
     }
+}
+
+const addTodo = todo => {
+    return {
+        type: 'ADD_TODO',
+        payload: { todo }
+    }
+}
+
+const completeTodo = id => {
+    return {
+        type: 'COMPLETE_TODO',
+        payload: { id }
+    }
+}
+
+const TodoEditor = _ => {
 
     const [todos, dispatchTodos] = useReducer(todosReducer, initialTodos)
-
-    const addTodo = todo => {
-        return {
-            type: 'ADD_TODO',
-            payload: { todo }
-        }
-    }
-
+    
     const dispatchAddTodo = todo => dispatchTodos(addTodo(todo))
-
-    const completeTodo = id => {
-        return {
-            type: 'COMPLETE_TODO',
-            payload: { id }
-        }
-    }
 
     const dispatchCompleteTodo = id => dispatchTodos(completeTodo(id))
 
